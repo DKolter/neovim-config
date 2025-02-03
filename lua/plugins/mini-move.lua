989d304f5b3ac150e7c1bd33b25333cdf930f404
DKolter <danielkolter157@gmail.com>
2025-02-03 11:05:12 +0100

    Switch to LazyVim configuration

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.move",
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        left = "H",
+        right = "L",
+        down = "J",
+        up = "K",
+      },
+    },
+  },
+}
