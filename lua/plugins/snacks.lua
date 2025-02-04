b8c5438b723dcbb7743ab0c495cd419d2f42838b
DKolter <danielkolter157@gmail.com>
2025-02-04 10:31:59 +0100

    Configure dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,17 @@ return {
     "folke/snacks.nvim",
     opts = {
       scroll = { enabled = false },
+      dashboard = {
+        sections = {
+          {
+            section = "terminal",
+            cmd = "pokemon-colorscripts -n pikachu --no-title; sleep .1",
+            indent = 20,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+      },
     },
   },
 }
